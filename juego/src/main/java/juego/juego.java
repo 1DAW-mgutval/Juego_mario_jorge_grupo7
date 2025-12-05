c47c984bf1b07f75949faff047f1bd572ea0c83d
1DAW-mgutval <mgutval@g.educaand.es>
2025-12-05 12:48:43 +0100

    2 funciones hechas

--- juego/src/main/java/juego/juego.java
+++ juego/src/main/java/juego/juego.java
@@ -1,19 +1,76 @@
 package juego;
 
+import java.util.Arrays;
+
 public class juego {
     public static void main(String[] args) {
-        
-    }
 
-    public static String pasarPalabraGuiones (String palabra) {
-
-    }
-
-    public static String formarPalabra (char letra, String palabra) {
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 
     public static boolean comprobarLetra (char letra, String palabra) {
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.charAt(i) == letra) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public static int[] posicionesLetra (char letra, String palabra) {
+        int posiciones[] = new int[1];
+        int contadorPosiciones = 0;
+        boolean primero = true;
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.charAt(i) == letra && !primero) {
+                Arrays.copyOf(posiciones, contadorPosiciones+1);
+                posiciones[contadorPosiciones] = i;
+            } else if (primero && palabra.charAt(i) == letra) {
+                posiciones[contadorPosiciones] = i;
+                contadorPosiciones++;
+            }
+        }
+        return posiciones;
+    }
+
+    public static String formarPalabra (char letra, String palabraGuiones, String palabra) {
+        for (int i = 0; i < palabraGuiones.length(); i++) {
+            if (comprobarLetra(letra, palabra)) {
+                
+            }
+        }
     }
 }
