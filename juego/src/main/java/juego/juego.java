f78ee5b310131dd8156ad58525cc6ee4d58aa0a4
1DAW-mgutval <mgutval@g.educaand.es>
2025-12-05 12:31:40 +0100

    funciones dichas

---
+++ juego/src/main/java/juego/juego.java
@@ -0,0 +1,19 @@
+package juego;
+
+public class juego {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String pasarPalabraGuiones (String palabra) {
+
+    }
+
+    public static String formarPalabra (char letra, String palabra) {
+
+    }
+
+    public static boolean comprobarLetra (char letra, String palabra) {
+
+    }
+}
